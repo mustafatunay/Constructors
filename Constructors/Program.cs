1ce79a4e0bd44ae5ddebee150a0050afdc690745
resultunay@gmail.com <resultunay@gmail.com>
2021-03-10 00:15:29 +0300

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Mustafa", LastName = "Tünay", City = "İstanbul" };
+
+            Customer customer3 = new Customer();
+            customer3.Id = 4;
+            
+            Customer customer2 = new Customer(2, "Burçak", "Tünay", "İstanbul");
+
+            Console.WriteLine(customer2.FirstName);
+            
+        }
+      
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //default constructor
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ ConstructorsExp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConstructorsExp
+{
+	public class Tasit
+	{
+
+		public int hiz; // Taşıtın Maximum hızı 
+		public String renk; // Taşıtın rengi
+		public String marka; // Taşıtın markası
+
+		// Parametreli yapılandırıcıda ilk değerli atıyoruz
+		public Tasit(int hiz, String marka, String renk)
+		{
+			this.marka = marka;
+			this.hiz = hiz;
+			this.renk = renk;
+		}
+
+		public void tasitInfo()
+		{
+			String tasit = "Taşıtın markası: " + marka + " rengi: " + renk + " maximum hızı: " + hiz;
+
+			System.Console.WriteLine(tasit);
+		}
+
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// Taşıt tipinden nesneler oluşturuyoruz
+			// Nesne oluşturuken yapılandırıcının istedği değerleri veriyoruz
+			Tasit otomobil = new Tasit(200, "Renault", "Mavi");
+
+			otomobil.tasitInfo();
+
+
+		}
+	}
+}
+
